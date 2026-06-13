887b3f7d36f8e96a321051fcb77b84b8df630d61
Nilson Rodrigo <nilsonrodrigues818@gmail.com>
2026-06-13 14:36:30 -0300

    feat: create login page layout

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,22 @@
+export function LoginPage() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label> Email</label>
+                    <input type="email" />
+                </div>
+
+                <div>
+                    <label >Senha</label>
+
+                    <input type="password" />
+                </div>
+                <button>
+                    Entrar
+                </button>
+            </form>
+        </div>
+    )
+}
